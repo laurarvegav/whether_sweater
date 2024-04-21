6502653ecd72ad53630417941c6ccbc887902afa
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-20 19:08:03 -0600

    Test: create tests for Location Service

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -2,38 +2,38 @@ require 'rails_helper'
 
 RSpec.describe LocationService do
 
-  describe '#valid?' do
-    it "returns true when valid parameter of city is given" do
-      search = LocationService.new("Denver, CO")
+  describe '.search' do
+    let!(:lat_1) {39.74001}
+    let!(:lng_1) {-104.99202}
 
-      expect(search.valid?).to eq(true)
-    end
-
-    it "returns false when invalid parameter of city is given" do
-      search = LocationService.new("Denver")
-
-      expect(search.valid?).to eq(false)
+    it "returns correct latitude and longitude", :vcr do
+      expect(LocationService.search("Denver,CO")).to eq({lat: lat_1, lng: lng_1})
     end
   end
 
-  describe '#search' do
-    let!(:lat_1) {"39.742043"}
-    let!(:lon_1) {"-104.991531"}
-    
-    let(:location_search) { LocationSearch.new(query) }
+  describe ".get_url" do
+    let!(:uri) { "v1/address?location=denver" }
+    it "returns correct response", :vcr do
+      data = LocationService.get_url(uri)
+      
+      expect(data).to be_a(Hash)
+      data[:results].each do |result|
 
-    context "with city in format: City, State_abreviation" do
-      let(:query) { "Denver, CO" }
-      it "returns correct latitude and longitude" do
-        expect(location_search.search(query)).to eq({lat: lat_1, lon: lon_1})
-      end
-
-      context "with name and city that are not valid" do
-        let(:query) { {state: "Colorado", city: "San Francisco"} }
-        it "returns markets matching the search parameters" do
-          expect(market_search.search).to eq([])
+        expect(result).to be_a(Hash)
+        result[:locations].each do |location|
+          expect(location).to be_a(Hash)
+          expect(location[:latLng]).to be_a(Hash)
+          expect(location[:latLng]).to have_key(:lat)
+          expect(location[:latLng]).to have_key(:lng)
         end
       end
+    end
+  end
+
+  describe ".connection" do
+    it "successfully connects to mapquest geocoding" do
+      connection = LocationService.connection
+      expect(connection).to be_a(Faraday::Connection)
     end
   end
 end
