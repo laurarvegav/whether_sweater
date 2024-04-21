92410fc1a3d270d5497d5f33e15113750490b2fd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 14:15:53 -0600

    Test: add sad path tests

--- spec/requests/api/v0/city_weather/retrieve_weather_for_a_city_spec.rb
+++ spec/requests/api/v0/city_weather/retrieve_weather_for_a_city_spec.rb
@@ -2,29 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Search Controller" do
   describe 'GET #search' do    
-    it 'returns the weather for a city', :vcr do
+    before do
       get "/api/v0/forecast?location=denver,co"
       expect(response.status).to eq(200)
 
-      response_data = JSON.parse(response.body, symbolize_names: true)
-      
-      expect(response_data[:id]).to be_nil
+      @response_data = JSON.parse(response.body, symbolize_names: true)
+    end
 
-      expect(response_data[:type]).to eq("forecast")
+    it 'returns the weather for a city', :vcr do
+      expect(@response_data[:id]).to be_nil
 
-      check_hash_structure(response_data[:attributes], :current_weather, Hash)
-      check_hash_structure(response_data[:attributes][:current_weather], :last_updated, String)
-      expect(human_readable?(response_data[:attributes][:current_weather][:last_updated])).to be(true)
-      check_hash_structure(response_data[:attributes][:current_weather], :temperature, Float)
-      check_hash_structure(response_data[:attributes][:current_weather], :feels_like, Float)
-      expect(response_data[:attributes][:current_weather][:humidity]).to be_a_kind_of(Float).or be_a_kind_of(Integer)
-      check_hash_structure(response_data[:attributes][:current_weather], :uvi, Float || Integer)
-      check_hash_structure(response_data[:attributes][:current_weather], :visibility, Float || Integer)
-      check_hash_structure(response_data[:attributes][:current_weather], :condition, String)
-      check_hash_structure(response_data[:attributes][:current_weather], :icon, String)
+      expect(@response_data[:type]).to eq("forecast")
 
-      check_hash_structure(response_data[:attributes], :daily_weather, Array)
-      response_data[:attributes][:daily_weather].each do |daily_weather|
+      check_hash_structure(@response_data[:attributes], :current_weather, Hash)
+      check_hash_structure(@response_data[:attributes][:current_weather], :last_updated, String)
+      expect(human_readable?(@response_data[:attributes][:current_weather][:last_updated])).to be(true)
+      check_hash_structure(@response_data[:attributes][:current_weather], :temperature, Float)
+      check_hash_structure(@response_data[:attributes][:current_weather], :feels_like, Float)
+      expect(@response_data[:attributes][:current_weather][:humidity]).to be_a_kind_of(Float).or be_a_kind_of(Integer)
+      check_hash_structure(@response_data[:attributes][:current_weather], :uvi, Float || Integer)
+      check_hash_structure(@response_data[:attributes][:current_weather], :visibility, Float || Integer)
+      check_hash_structure(@response_data[:attributes][:current_weather], :condition, String)
+      check_hash_structure(@response_data[:attributes][:current_weather], :icon, String)
+
+      check_hash_structure(@response_data[:attributes], :daily_weather, Array)
+      @response_data[:attributes][:daily_weather].each do |daily_weather|
         check_hash_structure(daily_weather, :date, String)
         expect(human_readable?(daily_weather[:date])).to be(true)
         check_hash_structure(daily_weather, :sunrise, String)
@@ -36,8 +38,8 @@ RSpec.describe "Search Controller" do
         check_hash_structure(daily_weather, :icon, String)
       end
 
-      check_hash_structure(response_data[:attributes], :hourly_weather, Array)
-      response_data[:attributes][:hourly_weather].each do |hourly_weather|
+      check_hash_structure(@response_data[:attributes], :hourly_weather, Array)
+      @response_data[:attributes][:hourly_weather].each do |hourly_weather|
         check_hash_structure(hourly_weather, :time , String)
         expect(human_readable?(hourly_weather[:time])).to be(true)
         expect(hourly_weather[:temperature]).to be_a_kind_of(Float).or be_a_kind_of(Integer)
@@ -46,8 +48,16 @@ RSpec.describe "Search Controller" do
       end
     end
 
-    xit 'does not return unnecessary data for a city' do
-      #Testing should also determine which fields should NOT be present. (don’t send unnecessary data)
+    it 'does not return unnecessary data for a city', :vcr do
+      expect(@response_data[:attributes][:current_weather]).not_to have_key(:is_day)
+      expect(@response_data[:attributes][:current_weather]).not_to have_key(:wind_mph)
+      expect(@response_data[:attributes][:current_weather]).not_to have_key(:wind_degree)
+
+      expect(@response_data[:attributes][:daily_weather][0]).not_to have_key(:maxwind_mph)
+      expect(@response_data[:attributes][:daily_weather][0]).not_to have_key(:avg_humidity)
+
+      expect(@response_data[:attributes][:hourly_weather][0]).not_to have_key(:wind_dir)
+      expect(@response_data[:attributes][:hourly_weather][0]).not_to have_key(:cloud)
     end
   end
 end
