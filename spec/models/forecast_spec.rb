d1e531350397150bacbc46ddda13d395ac44d27a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 14:00:48 -0600

    Fix tests Forecast model

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -12,13 +12,15 @@ RSpec.describe Forecast, type: :model do
       forecast = Forecast.new( 
         current_weather: 
         { temperature: 25, conditions: 'Sunny' },
-        daily_weather: { temperature_high: 30, temperature_low: 20 },
+        daily_weather: [{ temperature_high: 30, temperature_low: 20 }, { temperature_high: 12, temperature_low: 10 }],
         hourly_weather: [ { time: '10:00 AM', temperature: 27 }, { time: '11:00 AM', temperature: 29 } ]
       )
       
       expect(forecast.current_weather).to be_a(Hash)
-      expect(forecast.daily_weather).to be_a(Hash)
+      expect(forecast.daily_weather).to be_an(Array)
+      expect(forecast.daily_weather.all? { |daily| daily.is_a?(Hash)}).to be(true)
       expect(forecast.hourly_weather).to be_an(Array)
+      expect(forecast.hourly_weather.all? { |hourly| hourly.is_a?(Hash)}).to be(true)
     end
   end
 end
