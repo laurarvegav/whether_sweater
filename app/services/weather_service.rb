498003bdf9bdd3ec4e8de57439cbb406bec8a955
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 01:03:09 -0600

    Test: create weather service

---
+++ app/services/weather_service.rb
@@ -0,0 +1,22 @@
+class WeatherService
+  def self.connection
+    Faraday.new(url: "http://api.weatherapi.com/") do |faraday|
+      faraday.params["key"] = Rails.application.credentials.weather["key"]
+    end
+  end
+
+  def self.get_url(uri)
+    response = connection.get(uri)
+    
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.search(coordinates)
+    data = get_url("v1/forecast.json?q=#{coordinates}&days=5")
+    
+    { 
+      current: data[:current], 
+      forecast: data[:forecast][:forecastday] 
+    }
+  end
+end
