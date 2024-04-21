2f10e56a19061e0b8b3139574e94258e30f60f41
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 17:43:43 -0600

    Test: create tests for generate_api_key method for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
   end
+
+  describe "instance methods" do
+    before do
+      @user_data = {
+        email: "person@woohoo.com",
+        password: "abc123",
+        password_confirmation: "abc123"
+      }
+  
+      @bad_user_data_1 = {
+        email: "test@email.com",
+        password: "abc123",
+        password_confirmation: "abc12"
+      }
+    end
+
+    describe "#generate_api_key" do
+      it "creates api_key upon creation" do
+        user = User.create!(@user_data)
+  
+        expect(user.api_key).not_to be_nil
+      end
+    end
+  end
 end
