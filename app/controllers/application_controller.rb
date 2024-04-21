dbe2b8ec02e8d0c2f6b2a181acfd697ccecad086
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 17:45:01 -0600

    Feat: add rescue actions for record invalid and not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
+
+  def not_found(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
+  end
+
+  def invalid(exception)
+    if exception.message.include?("taken")
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 422)).serialize_json, status: 422
+    else 
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: 404
+    end 
+  end
 end
