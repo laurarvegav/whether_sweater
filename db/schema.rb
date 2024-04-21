30822b67e4ad2e45c77e175431a3bc610caa3ed2
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 17:02:16 -0600

    Feat: create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_20_220029) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_21_225947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_220029) do
     t.text "current_weather"
     t.text "daily_weather"
     t.text "hourly_weather"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
