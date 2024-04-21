90198a12bd19cfbb44ed2227505fea9dcf2d5dfd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 17:44:06 -0600

    Feat: create tests generate_api_key method for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
+require 'securerandom'
+
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { on: :create }, confirmation: true 
 
   has_secure_password
+  before_create :generate_api_key
+
+  def generate_api_key
+    self.api_key = SecureRandom.base64(24)
+  end
 end
