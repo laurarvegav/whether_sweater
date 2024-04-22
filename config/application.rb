355e21aa763cbd8f48548dc685212ad62cb69ce4
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 18:22:22 -0600

    Config: session and cookies configuration

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module WhetherSweater
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,9 +33,12 @@ module WhetherSweater
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
