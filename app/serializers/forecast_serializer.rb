85f0c73a08117d60ee91490beba074551e99818a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-21 23:25:59 -0600

    Refactor using serializer gem

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -4,15 +4,4 @@ class ForecastSerializer
   attributes :current_weather,
              :daily_weather,
              :hourly_weather
-
-  def self.format_forecast(city)
-    forecast = ServicesFacade.city_forecast(city)
-    {data:
-    {
-        id: nil,
-        type: forecast.class.to_s.downcase!,
-        attributes: forecast.attributes.except("id", "updated_at", "created_at")
-      }
-    }
-  end
 end
