b38da83bcdc298eb6e08da0a4b68d74edcf3e127
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-22 11:53:28 -0600

    Refactor: ermove unnecessary comments

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,20 +6,4 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of(:isbn) }
     it { should validate_presence_of(:publisher) }
   end
-
-  # describe "initialize" do
-  #   it "exists" do
-  #     title = "Denver, Co Vicinity (City Wall Maps)"
-  #     isbn = ["0762538627", "9780762538621"]
-  #     publisher = ["Universal Map Enterprises"]
-  #     book_data = {
-  #       title: title,
-  #       isbn: isbn,
-  #       publisher: publisher,
-  #     }
-  #     book = Book.new(book_data)
-      
-  #     expect(book).to be_a(Book)
-  #   end
-  # end
 end
