2908c5f97fea984be2afc921137836ed99250751
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-22 11:06:46 -0600

    Feat: create books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_21_225947) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_170126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "isbn", default: [], array: true
+    t.text "publisher", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "forecasts", force: :cascade do |t|
     t.text "current_weather"
