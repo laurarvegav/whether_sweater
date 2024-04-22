d59f44110908500bedde38c9afba402c97caa395
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-22 11:42:21 -0600

    Feat: create initialize method for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :isbn
   validates_presence_of :publisher
+
+  def initialize(data)
+    @title = data[:title]
+    @isbn = data[:isbn]
+    @publisher = data[:publisher]
+  end
 end
