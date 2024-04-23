cae22f8c9ab2c3918e5a9a1a0750d5836759af52
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 10:15:54 -0600

    Feat: create business service

---
+++ app/services/business_service.rb
@@ -0,0 +1,17 @@
+class BusinessService
+  def self.connection
+    Faraday.new(url: "https://api.yelp.com") do |faraday|
+      faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.yelp["key"]}"
+    end
+  end
+
+  def self.get_url(uri)
+    response = connection.get(uri)
+    
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.search(data)
+    data = get_url("v3/businesses/search?location=#{data[:destination]}&categories=#{data[:food]}&limit=1")
+  end
+end
