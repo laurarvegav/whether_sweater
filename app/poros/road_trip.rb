92816ac9d533cabccb620a15392abafe8c500d8a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 14:18:12 -0600

    Feat: createmethod RoadTrip poro weather at eta

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,13 +1,19 @@
 class RoadTrip
   attr_reader :origin,
               :duration,
-              :destination,
-              :forecast
+              :destination
           
   def initialize(data)
     @origin = data[:origin]
     @duration = data[:duration]
     @destination = data[:destination]
-    @forecast = data[:forecast]
+  end
+
+  def weather_at_eta(current_weather)
+    {
+      datetime: current_weather[:last_updated],
+      temperature: current_weather[:temperature],
+      condition: current_weather[:condition]
+    }
   end
 end
