f92ecce857c54dbe067e150a711a911df1408b71
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 01:11:13 -0600

    Fix location facade tests

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -1,14 +1,14 @@
-equire 'rails_helper'
+require 'rails_helper'
 
-RSpec.describe ServicesFacade do
+RSpec.describe LocationFacade do
   describe ".find_road_trip returns a hash with keys" do
     before do
       roadt_params = {origin: "denver,co", destination: "broomfield,co"}
-      @service = LocationFacade.find_road_trip(roadt_params)
+      @service = LocationFacade.road_trip_time(roadt_params)
     end
-    it "road_trip associated to a RoadTrip object" do
-      expect(@service[:travel_time]).to be_a(String)
-      expect(@service[:road_trip]).to be_a(RoadTrip)
+    it "time and duration associated to strings", :vcr do
+      expect(@service[:time]).to be_a(String)
+      expect(@service[:duration]).to be_a(ActiveSupport::Duration)
     end
   end
 end
