37c66ffc2a7bea69aa24d822aaff295bbefb64bd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 00:57:59 -0600

    Tests: Refactor location service tests and create road_trip test

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe LocationService do
-
-  describe '.search' do
-    let!(:lat_1) {39.74001}
-    let!(:lng_1) {-104.99202}
-
-    it "returns correct latitude and longitude", :vcr do
-      expect(LocationService.search("Denver,CO")).to eq({lat: lat_1, lng: lng_1})
+  
+  describe ".connection" do
+    it "successfully connects to mapquest geocoding", :vcr do
+      connection = LocationService.connection
+      expect(connection).to be_a(Faraday::Connection)
     end
   end
-
+  
   describe ".get_url" do
-    let!(:uri) { "v1/address?location=denver" }
+    let!(:uri) { "geocoding/v1/address?location=denver" }
     it "returns correct response", :vcr do
       data = LocationService.get_url(uri)
       
       expect(data).to be_a(Hash)
       data[:results].each do |result|
-
+        
         expect(result).to be_a(Hash)
         result[:locations].each do |location|
           expect(location).to be_a(Hash)
@@ -27,13 +25,27 @@ RSpec.describe LocationService do
           expect(location[:latLng]).to have_key(:lng)
         end
       end
+    end 
+  end
+
+  describe '.search' do
+    let!(:lat_1) {39.74001}
+    let!(:lng_1) {-104.99202}
+
+    it "returns correct latitude and longitude", :vcr do
+      data = LocationService.search("Denver,CO")
+      coordinates = data[:results][0][:locations][0][:latLng]
+      expect(coordinates).to eq({lat: lat_1, lng: lng_1})
     end
   end
 
-  describe ".connection" do
-    it "successfully connects to mapquest geocoding", :vcr do
-      connection = LocationService.connection
-      expect(connection).to be_a(Faraday::Connection)
+
+  describe '.road_trip' do
+    it "returns correct formatted time as the correct string", :vcr do
+      data = LocationService.road_trip("New York, NY", "Denver, CO")
+
+      check_hash_structure(data, :route, Hash)
+      expect(data[:route][:formattedTime]).to eq("24:51:19")
     end
   end
 end
