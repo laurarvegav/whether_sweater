4e5af4f7105a62078a393df1daa7da8323b9a936
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 01:11:33 -0600

    Fix location facade methods

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -1,12 +1,26 @@
+require 'active_support/all'
 class LocationFacade
 
   def self.format_coordinates(city)
-    coordinates = LocationService.search(city)
+    data = LocationService.search(city)
+    coordinates = data[:results].first[:locations].first[:latLng]
 
     coordinates.values.join(',')
   end
-  
-  def self.find_road_trip(road_trip_params)
+
+  def self.road_trip_time(params)
+    data = LocationService.road_trip(params[:origin], params[:destination])
     
+    time_string = data[:route][:formattedTime]
+    
+    hours, minutes, seconds = time_string.split(':').map(&:to_i)
+
+    # Construct a ActiveSupport::Duration object
+    duration = 1.day + hours.hours + minutes.minutes + seconds.seconds
+
+    {
+      time: time_string,
+      duration: duration
+    }
   end
 end
