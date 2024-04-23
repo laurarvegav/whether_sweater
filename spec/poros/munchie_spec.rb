f43f98b7699fe5557312e5b0fe35fef458933784
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 10:15:15 -0600

    Fix typo

--- spec/poros/munchie_spec.rb
+++ spec/poros/munchie_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Munchie do
   describe 'Initialize' do
     it "exists and populates attribures corerctly" do
       munchie = Munchie.new( 
-        { name: "Tavernetta", review_count: 693, raiting: 4.5, location: 
+        { name: "Tavernetta", review_count: 693, rating: 4.5, location: 
           {display_address: [
             "1889 16th St",
             "Denver, CO 80202"
@@ -18,7 +18,7 @@ RSpec.describe Munchie do
       expect(munchie.name).to eq("Tavernetta")
       
       expect(munchie.address).to eq("1889 16th St, Denver, CO 80202")
-      expect(munchie.raiting).to eq(4.5)
+      expect(munchie.rating).to eq(4.5)
       expect(munchie.reviews).to eq(693)
     end
   end
