8c7173be195d70b6b1c5d2cd6d3b23ecab2ed393
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-22 21:20:50 -0600

    Fix test data

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -3,35 +3,17 @@ require 'rails_helper'
 RSpec.describe RoadTrip do
   describe 'instance methods' do
     before do
-    @roadt_data = {
-      "origin": "Cincinatti,OH",
-      "destination": "Chicago,IL",
-      "api_key": @user_1.api_key
-    }
+      @user_1 = User.create!({
+        email: "person3@woohoo.com",
+        password: "abc123",
+        password_confirmation: "abc123"
+      })
 
-    @bad_roadt_data_0 = {
-      "origin": "Cincinatti,OH",
-      "destination": "Chicago,IL",
-      "api_key": ""
-    }
-
-    @bad_roadt_data_1 = {
-      "origin": "",
-      "destination": "Chicago,IL",
-      "api_key": @user_1.api_key
-    }
-
-    @bad_roadt_data_2 = {
-      "origin": "Cincinatti,OH",
-      "destination": "",
-      "api_key": @user_1.api_key
-    }
-
-    @bad_roadt_data_3 = {
-      "origin": "New York, NY",
-      "destination": "London, UK",
-      "api_key": @user_1.api_key
-    }
+      @roadt_data = {
+        "origin": "Cincinatti,OH",
+        "destination": "Chicago,IL",
+        "api_key": @user_1.api_key
+      }
     end
 
     describe "#initialize" do
@@ -40,7 +22,7 @@ RSpec.describe RoadTrip do
 
         expect(road_trip).to be_a(RoadTrip)
         expect(road_trip.start_city).to eq("Cincinatti,OH")
-        expect(road_trip.end_city).to eq("Chicago, IL")
+        expect(road_trip.end_city).to eq("Chicago,IL")
       end
     end
   end
