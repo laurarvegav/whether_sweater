8256746aaaf7ba5a4ce972605d453ee0f2aa6604
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 14:52:27 -0600

    fix reoad trips controller actions to handle edge case impossible trip

--- app/controllers/api/v0/road_trips_controller.rb
+++ app/controllers/api/v0/road_trips_controller.rb
@@ -2,37 +2,66 @@ class Api::V0::RoadTripsController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
 
-    if user && road_trip_params.include?(nil)
-      output = {
-        data: {
-          id: nil,
-          type: "road_trip",
-          attributes: road_trip
+    if user 
+      if road_trip_params[:origin].blank?
+        render_error("Validation failed: origin incorrect", 422)
+      elsif road_trip_params[:destination].blank?
+        render_error("Validation failed: destination incorrect", 422)
+      else
+
+        road_trip = RoadTrip.new(
+          origin: params[:origin],
+          destination: params[:destination],
+          duration: trip_duration,
+          forecast: road_trip_weather
+          )
+          
+        output = {
+          data: {
+            id: nil,
+            type: "road_trip",
+            attributes: road_trip_attributes(road_trip)
+          }
         }
-      }
- 
-    render json: output, status: 201
+          
+        render json: output, status: 201
+      end
     else
       render json: ErrorSerializer.new(ErrorMessage.new("Unauthorized", 422)).serialize_json, status: 401
     end
   end
 
   private
-  def road_trip
-    time = LocationFacade.road_trip_time(road_trip_params)
-    coordinates = LocationFacade.format_coordinates(params[:destination])
 
-    weather_at_eta = WeatherFacade.road_trip_weather(coordinates, time[:duration])
+  def trip_duration
+    LocationFacade.road_trip_time(road_trip_params)[:duration]
+  end
 
-    {
-      start_city: params[:origin], 
-      end_city: params[:destination],
-      travel_time: time[:time],
-      weather_at_eta: weather_at_eta
+  def road_trip_weather
+      coordinates = LocationFacade.format_coordinates(params[:destination])
+      WeatherFacade.city_forecast(coordinates, trip_duration).current_weather
+  end
+
+  def road_trip_attributes(road_trip)
+    attributes = {
+      start_city: road_trip.origin,
+      end_city: road_trip.destination,
+      travel_time: road_trip.duration,
     }
+
+    if trip_duration != "impossible"
+      attributes[:weather_at_eta] = road_trip.weather_at_eta(road_trip_weather)
+    else
+      attributes[:weather_at_eta] = {}
+    end
+    attributes
   end
 
   def road_trip_params
     params.permit(:origin, :destination)
+  end
+
+  def render_error(detail, status)
+    render json: ErrorSerializer.new(ErrorMessage.new(detail, status)).serialize_json, status: status
   end
 end
