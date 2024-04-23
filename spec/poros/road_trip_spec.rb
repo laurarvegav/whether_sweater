5eb7b56b9fce1baf3812b68364fcc69112b19beb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-23 14:17:44 -0600

    Test: create tests for method RoadTrip poro weather at eta

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -11,18 +11,33 @@ RSpec.describe RoadTrip do
         temperature: 44.2,
         condition: "Cloudy with a chance of meatballs"
       }
+    } 
+
+    @forecast_data = {
+      last_updated: "2023-04-07 23:00",
+      temperature: 44.2,
+      condition: "Cloudy with a chance of meatballs"
     }
+    
+    @road_trip = RoadTrip.new(@data)
   end
 
   describe "#initialize" do
     it "exists and populates atrtibutes properly" do
-      road_trip = RoadTrip.new(@data)
 
-      expect(road_trip).to be_a(RoadTrip)
-      expect(road_trip.origin).to eq(@data[:origin])
-      expect(road_trip.duration).to eq(@data[:duration])
-      expect(road_trip.destination).to eq(@data[:destination])
-      expect(road_trip.forecast).to eq(@data[:forecast])
+      expect(@road_trip).to be_a(RoadTrip)
+      expect(@road_trip.origin).to eq(@data[:origin])
+      expect(@road_trip.duration).to eq(@data[:duration])
+      expect(@road_trip.destination).to eq(@data[:destination])
+      
+    end
+  end
+
+  describe "#weather_at_eta" do
+    it "formats the current weather forecast received as parameter" do
+      weather_at_eta = @road_trip.weather_at_eta(@forecast_data)
+      
+      expect(weather_at_eta).to eq(@data[:forecast])
     end
   end
 end
